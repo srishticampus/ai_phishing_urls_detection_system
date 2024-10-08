07ffdbcfa86edd0675ddc38d96dc7a1f40d49890
syampencraft <syamlal.sies@gmail.com>
2024-10-08 12:31:02 +0530

    registeration,login page

--- blog_sphere/accounts/admin.py
+++ blog_sphere/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reg
 
-# Register your models here.
+@admin.register(Reg)
+class RegAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email', 'phone_number', 'age', 'interest')
+    search_fields = ('user__username', 'email', 'phone_number')
---
+++ blog_sphere/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from . import views
+
+urlpatterns = [
+    path('register/', views.reg, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('home/', views.home, name='home'),
+    # Add other URLs as needed
+]
