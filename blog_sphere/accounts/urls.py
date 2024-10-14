fd4969b535cf2010da9f08df4be963503dc36f48
Jithin Jose <jithinjose.sics@gmail.com>
2024-10-14 18:39:20 +0530

    project structure set , core app for common pages

--- blog_sphere/accounts/urls.py
+++ blog_sphere/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from . import views
 
+app_name = 'accounts'
 urlpatterns = [
     path('register/', views.reg, name='register'),
     path('login/', views.login_view, name='login'),
