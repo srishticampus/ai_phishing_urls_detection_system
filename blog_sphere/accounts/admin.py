7e1e943bf2ab5e2ce663ef1c1773f58d92a007a7
srishticampus <151642773+srishticampus@users.noreply.github.com>
2024-11-14 15:08:57 +0530

    Delete blog_sphere directory

--- blog_sphere/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Reg
-
-@admin.register(Reg)
-class RegAdmin(admin.ModelAdmin):
-    list_display = ('user', 'email', 'phone_number', 'age', 'interest')
-    search_fields = ('user__username', 'email', 'phone_number')
