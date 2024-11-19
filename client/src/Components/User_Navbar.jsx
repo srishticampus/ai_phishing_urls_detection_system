d958f0175bb66550f2dc6401fbf9351f3155d9a3
Anandhu <anandhujs.srishti@gmail.com>
2024-11-19 12:48:44 +0530

    forgetP pull

---
+++ client/src/Components/User_Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/Styles/Components_Styles/User_Navbar.css'
+function User_Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  )
+}
+
+export default User_Navbar
