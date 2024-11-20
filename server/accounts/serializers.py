44d847883a8d5c57d94927f672a65a879bdbf331
Jithin Jose <jithinjose.sics@gmail.com>
2024-11-20 11:34:38 +0530

    Create Profile api created

--- server/accounts/serializers.py
+++ server/accounts/serializers.py
@@ -1,9 +1,18 @@
 #serializers.py accounts
+"""
+This module contains serializers for the accounts app.
+
+It defines serializers for the user profile and related models, enabling data
+serialization and deserialization for API requests and responses. This includes
+handling custom fields and business logic, such as linking user profiles with
+authenticated users.
+"""
+
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
-from .models import User
+from .models import User,Profile
 #Serializer for the User Model
 class UserSerializer(serializers.ModelSerializer):
     """
@@ -72,3 +81,34 @@ class ResetPasswordSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         raise NotImplementedError("Update method not implemented")
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for profile details of a user, including first_name and last_name.
+    """
+    first_name = serializers.CharField(write_only=True)
+    last_name = serializers.CharField(write_only=True)
+    photo = serializers.ImageField(required=False)
+    class Meta:
+        model = Profile
+        fields = ['phone_number', 'gender', 'photo', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        """
+        Overriding create to ensure first_name and last_name are updated in the User model
+        """
+        user = self.context['request'].user
+        validated_data['user'] = user
+
+        # Extract first_name and last_name from validated data
+        first_name = validated_data.pop('first_name', None)
+        last_name = validated_data.pop('last_name', None)
+
+        # Update the user's first_name and last_name
+        if first_name:
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+        user.save()
+
+        return super().create(validated_data)
