3a42b0ca9e7fd984c5f50b60ae0fbbcb572e7f84
Anandhu <anandhujs.srishti@gmail.com>
2025-01-07 14:56:42 +0530

    f

--- client/src/Pages/AdminLogin/AdminLogin.jsx
+++ client/src/Pages/AdminLogin/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import "../../Pages/AdminLogin/AdminLogin.css"
 import backgroundimg from "../../assets/Images/Login_Background.png"
 import { useState } from "react";
-import 'font-awesome/css/font-awesome.min.css';  // Import Font Awesome
+import 'font-awesome/css/font-awesome.min.css'; 
 
 function AdminLogin() {
     const [showPassword, setShowPassword] = useState(false);
@@ -11,9 +11,9 @@ function AdminLogin() {
     }
 
     return (
-        <div className="d-flex">
+        <div className="d-flex admin-login-container">
             <div className="admin-login-section-left">
-                <img src={backgroundimg} alt="Login Background" />
+                <img className="admin-login-image" src={backgroundimg} alt="Login Background" />
             </div>
             <div className="admin-login-section-right">
                 <p className="admin-login-right-head">Login</p>
