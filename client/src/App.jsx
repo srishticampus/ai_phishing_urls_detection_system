0a88327f4f870cde6ec4a8ac39137aec93680c2f
Anandhu <anandhujs.srishti@gmail.com>
2025-02-18 17:32:49 +0530

    Advertiser view advertisement details api integrated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"; // Add useState and useEffect
-import { useState, useEffect } from "react"; // Add useState and useEffect
+
 import { Routes, Route } from "react-router";
 import { ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -53,9 +53,9 @@ import UserViewDetails from "./Pages/UserViewDetails/UserViewDetails";
 import UserResetPassword from "./Pages/UserResetPassword/UserResetPassword";
 import UserAreaOfInterests from "./Pages/UserAreaOfInterests/UserAreaOfInterests";
 import UserViewAdvertisementDetails from "./Pages/UserViewAdvertisementDetails/UserViewAdvertisementDetails";
-import AdvertisersRegistration from "./Pages/AdvertisersRegistration/AdvertisersRegistration";
+// import AdvertisersRegistration from "./Pages/AdvertisersRegistration/AdvertisersRegistration";
 import { checkLoginStatus } from "./Services/apiService"; // Import the checkLoginStatus function
-import { checkLoginStatus } from "./Services/apiService"; // Import the checkLoginStatus function
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(checkLoginStatus()); // Track login status
