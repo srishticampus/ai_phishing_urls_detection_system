db0d557eafe0b3e41a8d80993bd29c0110c14b35
Anandhu <anandhujs.srishti@gmail.com>
2025-02-18 09:01:56 +0530

    Admin Add Blog api fixed

--- client/src/Pages/AdminViewBlog/AdminViewBlog.jsx
+++ client/src/Pages/AdminViewBlog/AdminViewBlog.jsx
@@ -1,9 +1,8 @@
 import "../../Pages/AdminViewBlog/AdminViewBlog.css";
-// import LandingP_card_one from '../../assets/Images/LandingP_card_one.png';
 import tabler_photo from '../../assets/Images/tabler_photo.png';
-import LandingPage_Bg from '../../assets/Images/LandingPage_Bg.png';
 import { viewBlogs } from "../../Services/apiService";
 import { useState, useEffect } from 'react';
+const baseUrl = import.meta.env.VITE_API_URL;
 
 function AdminViewBlog() {
 
@@ -17,6 +16,7 @@ function AdminViewBlog() {
                 console.log('Full API response:', response);
                 setBlogs(Array.isArray(response.data) ? response.data : []);
                 console.log(response.data);
+                console.log("blogs",blogs)
             } catch (error) {
                 console.error("Error fetching blogs:", error);
             } finally {
@@ -37,15 +37,19 @@ function AdminViewBlog() {
                 <p className="admin-home-container-head ms-3">View Blogs</p>
                 <div className="d-flex justify-content-center">
                     <div className="row">
-                        {/* If no blogs, show "No Blogs Added" */}
+   
                         {blogs.length === 0 ? (
                             <div className="col-12">
-                                <p>No Blogs Added</p>
+                                <p>No Blogs Added OR Check Authorization</p>
                             </div>
                         ) : (
+                          
                             blogs.map((blog) => (
                                 <div key={blog.id} className="card col-sm-3 admin-view-cardsize">
-                                    <img className="admin-view-blog-cardone-imgone" src={LandingPage_Bg} alt="Blog cover" />
+                                    {/* <img className="admin-view-blog-cardone-imgone" src={blog.image} alt="Blog cover" /> */}
+                                    <img className="admin-view-blog-cardone-imgone" src={`${baseUrl}${blog.image}`} alt="Blog cover" />
+
+
                                     <div>
                                         <span>
                                             <p className="badge p-2 admin-view-blog-cardone-header">
@@ -58,11 +62,11 @@ function AdminViewBlog() {
                                         <p className="admin-view-blog-cardone-body">{blog.title || 'Title'}</p>
                                         <div className="d-flex">
                                             <div className="d-flex landingpage-card-profile-info">
-                                                <p className="profile-textcolor">{blog.date || 'Date'}</p>
+                                                <p className="profile-textcolor">{"june 28,2018"}</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <p className="admin-view-blog-cardone-para">{blog.description || 'Blog description goes here.'}</p>
+                                    <p className="admin-view-blog-cardone-para">{blog.content || 'Blog description goes here.'}</p>
                                     <button className="btn admin-view-blog-readmore-button">
                                         Read More <span className="greaterthan-symbol">&gt;</span>
                                     </button>
